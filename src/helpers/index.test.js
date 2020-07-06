dba0848e79a2ea04c2d79d1e5e2ab49cde5141aa
Maksym Goncharov <maksym.g@stms.co.il>
2020-07-06 11:14:20 +0300

    add getLetterMatchCount method and tests

---
+++ src/helpers/index.test.js
@@ -0,0 +1,22 @@
+import { getLetterMatchCount } from './index';
+
+describe('getLetterMatchCount', () => {
+  const secretWord = 'party';
+  test('should return correct count when there are no matching letters', () => {
+    const letterMatchCount = getLetterMatchCount('bones', secretWord);
+
+    expect(letterMatchCount).toBe(0);
+  });
+
+  test('should return correct count when there are 3 matching letters', () => {
+    const letterMatchCount = getLetterMatchCount('train', secretWord);
+
+    expect(letterMatchCount).toBe(3);
+  });
+
+  test('should return correct count when there are duplicate letters', () => {
+    const letterMatchCount = getLetterMatchCount('parka', secretWord);
+
+    expect(letterMatchCount).toBe(3);
+  });
+});
